67a24ba31bd425ce473311c43b4f14590d893659
Romulo Cesar Silvestre Leite <romulo.leite@edu.senac.br>
2023-09-12 20:24:40 -0300

    salvando desafio para o Holdai

---
+++ js/ds1.js
@@ -0,0 +1,29 @@
+//aprendendo a repetição
+/*
+  Os algarismos para a formação dos números na base decimal são:
+  0 1 2 3 4 5 6 7 8 9 
+*/
+
+//loop
+//looping
+//repetição
+//iteração
+
+function showNumbers() {
+    let contador = 0 //inicio
+    while (contador < 10) { //condição
+        alert(contador) //algoritmo
+        contador = contador + 1 //incremento
+    }
+}
+
+function showNumbersFor(){    
+   //ETAPA 1 - inicio 
+   //ETAPA 2 - condição
+   //ETAPA 3 - algoritmo
+   //ETAPA 4 - incremento  
+   for(let contador=0;contador<10;contador++){              
+       alert(contador) 
+   }
+   
+}
---
+++ js/ds2.js
@@ -0,0 +1,33 @@
+/* 
+
+  Mostre apenas os números pares
+  
+  inicio = 2
+  condição = até 12
+  incremento = ????
+*/
+function evenNumbers(){
+  let numeros = 2 
+  while (numeros < 12 ) {
+      alert(numeros)
+      numeros = numeros + 2
+  }
+
+    
+}
+
+//desafio 3
+function oddNumbers(){
+  let numerosim = 1
+  while (numerosim < 13) {
+      alert(numerosim)
+      numerosim = numerosim + 2
+  }
+
+}
+
+//os três algoritmos usando a estrutura para
+
+//enquanto - while
+//para -  for
+//repita - do while
